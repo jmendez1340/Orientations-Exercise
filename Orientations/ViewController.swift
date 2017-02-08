9899fc973ae1ca1b1ea3a961f6b0f7d433b890f8
test <jmendez1340@e112-m-06.mchenry.edu>
2017-02-08 16:00:22 -0600

    Initial Commit!

--- Orientations/ViewController.swift
+++ Orientations/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask(rawValue:
+        (UIInterfaceOrientationMask.portrait.rawValue
+        | UIInterfaceOrientationMask.landscapeLeft.rawValue))
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
 
 }
 
